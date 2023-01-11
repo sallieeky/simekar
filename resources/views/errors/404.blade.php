a3715ec4f8d4c5c550bca058b2aef5c5af49740a
sallixie <10191077@student.itk.ac.id>
2023-01-11 14:26:55 +0800

    add 404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr" data-theme="light">
+
+<head>
+	<!-- BEGIN Import Firebase Auth Scripts -->
+	<!-- BEGIN Import Analytics Scripts -->
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width,initial-scale=1.0">
+	<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600&amp;family=Roboto+Mono&amp;display=swap" rel="stylesheet">
+	<link href="/dashboard_assets/assets/build/styles/ltr-core.css" rel="stylesheet">
+	<link href="/dashboard_assets/assets/build/styles/ltr-vendor.css" rel="stylesheet">
+	<link href="/dashboard_assets/assets/images/favicon.ico" rel="shortcut icon" type="image/x-icon">
+	<title>Not Found | SIMEKAR</title>
+</head>
+
+<body class="preload-active">
+	<!-- BEGIN Preload -->
+	<div class="preload">
+		<div class="preload-dialog">
+			<!-- BEGIN Spinner -->
+			<div class="spinner-border text-primary preload-spinner"></div>
+			<!-- END Spinner -->
+		</div>
+	</div>
+	<!-- END Preload -->
+	<!-- BEGIN Page Holder -->
+	<div class="holder">
+		<!-- BEGIN Page Wrapper -->
+		<div class="wrapper ">
+			<!-- BEGIN Page Content -->
+			<div class="content">
+				<div class="container-fluid g-4">
+					<div class="row g-0 align-items-center justify-content-center h-100">
+						<div class="col-md-8 col-lg-6 col-xl-4 text-center">
+							<h1 class="widget20">404</h1>
+							<h2 class="mb-3">Halaman Tidak Ditemukan!</h2>
+							<p class="mb-4">Mohon maaf halaman yang anda cari tidak dapat ditemukan, coba periksa kembali url yang anda masukkan. Terima Kasih</p>
+                <a href="/" class="btn btn-label-primary btn-lg btn-widest">Kembali ke Halaman Utama</a>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- END Page Content -->
+		</div>
+		<!-- END Page Wrapper -->
+	</div>
+	<!-- END Page Holder -->
+	<!-- BEGIN Float Button -->
+	<div class="floating-btn floating-btn-end d-grid gap-2">
+		<button class="btn btn-flat-primary btn-icon" data-bs-toggle="tooltip" data-bs-placement="right" title="Change theme" id="theme-toggle">
+			<i class="fa fa-moon"></i>
+		</button>
+		<a class="btn btn-flat-primary btn-icon" data-bs-toggle="tooltip" data-bs-placement="right" title="Look documentation" href="https://docs.blueupcode.com/guide-html_v2-0-0.html" target="_blank">
+			<i class="fa fa-book"></i>
+		</a>
+	</div>
+	<!-- END Float Button -->
+	<script type="text/javascript" src="/dashboard_assets/assets/build/scripts/mandatory.js"></script>
+	<script type="text/javascript" src="/dashboard_assets/assets/build/scripts/core.js"></script>
+	<script type="text/javascript" src="/dashboard_assets/assets/build/scripts/vendor.js"></script>
+	<script type="text/javascript" src="/dashboard_assets/assets/app/utilities/sticky-header.js"></script>
+	<script type="text/javascript" src="/dashboard_assets/assets/app/utilities/copyright-year.js"></script>
+	<script type="text/javascript" src="/dashboard_assets/assets/app/utilities/theme-switcher.js"></script>
+	<script type="text/javascript" src="/dashboard_assets/assets/app/utilities/tooltip-popover.js"></script>
+	<script type="text/javascript" src="/dashboard_assets/assets/app/utilities/dropdown-scrollbar.js"></script>
+	<script type="text/javascript" src="/dashboard_assets/assets/app/utilities/fullscreen-trigger.js"></script>
+</body>
+
+</html>
