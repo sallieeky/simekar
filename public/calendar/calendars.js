15c8c219d5519d310e747cee7c88f08c9937d75f
sallixie <10191077@student.itk.ac.id>
2023-02-20 23:12:28 +0800

    fix calendar

--- public/calendar/calendars.js
+++ public/calendar/calendars.js
@@ -78,4 +78,15 @@ function hexToRGBA(hex) {
     calendar.dragBgColor = "#ff5583";
     calendar.borderColor = "#ff5583";
     addCalendar(calendar);
+
+    calendar = new CalendarInfo();
+    id += 1;
+    calendar.id = String(id);
+    calendar.name = "Tanggal Servis Rutin";
+    calendar.category = "tgl_service_rutin";
+    calendar.color = "#ffffff";
+    calendar.bgColor = "#9e5fff";
+    calendar.dragBgColor = "#9e5fff";
+    calendar.borderColor = "#9e5fff";
+    addCalendar(calendar);
 })();
